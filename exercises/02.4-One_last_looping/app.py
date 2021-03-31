d14c176d28837268844af0d0b9c96946108ddc78
josnavch <josnavch@gmail.com>
2021-03-31 00:31:16 +0000

    Exercises 2-2.4 JN

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,8 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
-
+my_sample_list[1] = "Steven"
+my_sample_list[len(my_sample_list)-1] = "Pepe"
+my_sample_list[0] = my_sample_list[2] + my_sample_list[4]
+my_sample_list.reverse()
+print (my_sample_list)
