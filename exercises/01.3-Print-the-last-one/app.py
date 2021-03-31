a4feb9359aa90e475ce3fbd61b21fd0c9c581fda
josnavch <josnavch@gmail.com>
2021-03-31 00:11:05 +0000

    Finish exercises 1 JN

--- exercises/01.3-Print-the-last-one/app.py
+++ exercises/01.3-Print-the-last-one/app.py
@@ -12,3 +12,5 @@ def generate_random_list():
 my_stupid_list = generate_random_list()
 
 # Feel happy to write the code below this comment, good luck!:
+the_last_one = len(my_stupid_list)
+print (my_stupid_list[the_last_one - 1])
--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,10 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+i=0
+for i in range(1,11):
+    my_list.append(random.randint(1,7895))
+print(my_list)
